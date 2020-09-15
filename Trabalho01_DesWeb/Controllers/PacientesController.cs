1e186abd5f5a00c353735505c8a5920756afe887
Jean <jeanramos83@gmail.com>
2020-09-15 19:32:40 -0300

    Entrega do Trabalho

---
+++ Trabalho01_DesWeb/Controllers/PacientesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Trabalho01_DesWeb.Models;
+
+namespace Trabalho01_DesWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PacientesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("Listagem de Pacientes");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok("Busca de paciente");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(DadosPacientes dados)
+        {
+            return Ok("Registrar novo paciente");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(int id, DadosPacientes dados)
+        {
+            return Ok("Paciente atualizado com sucesso!");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok("Paciente removido com sucesso!");
+        }
+    }
+}
---
+++ Trabalho01_DesWeb/Models/DadosPacientes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trabalho01_DesWeb.Models
+{
+    public class DadosPacientes
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public int idade { get; set; }
+        public string cidade { get; set; }
+        public string estado { get; set; }
+        public string sexo { get; set; }
+        public string estadoCivil { get; set; }
+        public bool possuiComorbidade { get; set; }
+        public DateTime data { get; set; }
+
+    }
+}
